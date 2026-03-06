648a75c0d469c2186dd9280b53f719578944fcce
yohanim <kevin.gossent@gmail.com>
2026-03-06 14:26:53 +0100

    fix: imports

--- custom_components/daikin_local/oauth2.py
+++ custom_components/daikin_local/oauth2.py
@@ -5,16 +5,18 @@ from __future__ import annotations
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import config_entry_oauth2_flow
 
-from .const import DAIKIN_ONECTA_AUTH_URL, DAIKIN_ONECTA_TOKEN_URL
+from .const import DAIKIN_ONECTA_AUTH_URL, DAIKIN_ONECTA_TOKEN_URL, DOMAIN
 
 async def async_register_implementation(hass: HomeAssistant) -> None:
     """Register Daikin Onecta implementation."""
+    # This allows the integration to use the "Application Credentials" configuration
+    # where the user can enter their Client ID and Client Secret in the HA UI.
     config_entry_oauth2_flow.async_register_implementation(
         hass,
-        "daikin_local",
+        DOMAIN,
         config_entry_oauth2_flow.LocalOAuth2Implementation(
             hass,
-            "daikin_local",
+            DOMAIN,
             "Daikin Onecta",
             DAIKIN_ONECTA_AUTH_URL,
             DAIKIN_ONECTA_TOKEN_URL,
