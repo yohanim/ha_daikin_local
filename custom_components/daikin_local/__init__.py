a92d9bffea42fa12856fe9b8cedf6520b64f28f4
yohanim <kevin.gossent@gmail.com>
2026-03-06 14:03:49 +0100

    fix: load error

--- custom_components/daikin_local/__init__.py
+++ custom_components/daikin_local/__init__.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 import asyncio
 import logging
+from typing import Any
 
 from aiohttp import ClientConnectionError
 from pydaikin.daikin_base import Appliance
@@ -22,8 +23,8 @@ from homeassistant.exceptions import ConfigEntryNotReady
 from homeassistant.helpers import (
     device_registry as dr,
     entity_registry as er,
-    config_entry_oauth2_helper,
 )
+from homeassistant.helpers import config_entry_oauth2_helper
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.device_registry import CONNECTION_NETWORK_MAC
 from homeassistant.util.ssl import client_context_no_verify
